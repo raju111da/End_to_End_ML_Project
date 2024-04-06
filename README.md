2be9d6f83db365540792a5fdff94633516813650
Raju K <raju999da@gmail.com>
2024-04-06 17:06:39 +0530

    commit changes

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # End_to_End_ML_Project
+
+git clone https://github.com/raju111da/End_to_End_ML_Project.git
+
+conda create -p venv python==3.10
+
+conda activate venv/
+
+python template_FolderStructure.py
+
+pip install -r requirements.txt
+
+
+## Workflows
+
+update src/{Project}/logger.py
+update src/{Project}/utils/common.py
+
+update config.yaml
+update schema.yaml
+update params.yaml
+update the src/entity/config_entity.py
+update the src/config/configuration_manager
+update the components
+update the pipeline
+update the main.py
+update the app.py
+
---
+++ main.py
@@ -0,0 +1,66 @@
+from src.wine_quality import logger
+from src.wine_quality.pipeline.Stage01_DataIngestion import DataIngestionTrainingPipeline
+from src.wine_quality.pipeline.Stage02_DataValidation import DataValidationTrainingPipeline
+from src.wine_quality.pipeline.Stage03_DataTransformation import DataTransformationTrainingPipeline
+from src.wine_quality.pipeline.Stage04_ModeTrainer import ModelTrainerTrainingPipeline
+from src.wine_quality.pipeline.Stage05_ModelEvaluation import ModelEvaluationTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
